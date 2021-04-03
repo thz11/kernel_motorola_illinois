12bb2bf70062177af7d384a197d4ce075018c88a
Erfan Abdi <erfangplus@gmail.com>
2021-04-04 00:24:38 +0430

    drm: display: Add HBM sysfs node
    Change-Id: I9e3af98ce3bb0b178ee5cda0c784d57d929e62e7
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -324,6 +324,8 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
+
+	bool hbm_state;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -461,5 +463,7 @@ void dsi_panel_reset_param(struct dsi_panel *panel);
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
+
+void dsi_panel_set_custom_param(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
